2a629670589fc14d5d2513e718b9358fc083f644
Gareth Macdonald <macdonald.gareth@gmail.com>
2022-11-03 16:08:23 +0100

    Add client components folder with ProductDetails base

--- src/routes/products/[handle].server.tsx
+++ src/routes/products/[handle].server.tsx
@@ -1,13 +1,56 @@
-import { useRouteParams } from "@shopify/hydrogen";
+import { Suspense } from "react";
+import {
+  gql,
+  useShopQuery,
+  useServerAnalytics,
+  useRouteParams,
+  ShopifyAnalyticsConstants,
+  Seo,
+} from "@shopify/hydrogen";
 import { BaseLayout } from "../../components/layouts/BaseLayout.server/index";
+
+//types
+import { Product as TProduct } from "@shopify/hydrogen/storefront-api-types";
+
+const PRODUCT_QUERY = gql`
+  query Product($language: LanguageCode, $handle: String!)
+  @inContext(language: $language) {
+    product(handle: $handle) {
+      id
+      title
+      seo {
+        title
+        description
+      }
+    }
+  }
+`;
 
 function Product() {
   const { handle } = useRouteParams();
-  console.log("loaded");
+
+  const {
+    data: { product },
+  } = useShopQuery<{ product: TProduct }>({
+    query: PRODUCT_QUERY,
+    variables: {
+      handle,
+    },
+  });
+
+  useServerAnalytics({
+    shopify: {
+      pageType: ShopifyAnalyticsConstants.pageType.product,
+      resourceId: product.id,
+    },
+  });
   return (
     <BaseLayout>
+      <Suspense>
+        <Seo type="product" data={product} />
+      </Suspense>
       <section>
-        This will be the product page for <strong>{handle}</strong>
+        This will be the product page for <strong>{product.title}</strong>
       </section>
     </BaseLayout>
   );
