a4e661749308dcfce65e065dec12c6d82cbb22a1
Gareth Macdonald <macdonald.gareth@gmail.com>
2022-11-04 12:13:33 +0100

    Basic Gallery added, no interactivity

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,10 +1,21 @@
 import { Product } from "@shopify/hydrogen/storefront-api-types";
-import { Image } from "@shopify/hydrogen/storefront-api-types";
+import {
+  Image,
+  MediaImage,
+  ExternalVideo,
+  Video,
+  Model3d,
+} from "@shopify/hydrogen/storefront-api-types";
+
 /*
  * Base
  */
-
 export type TImage = Image;
+export type TMediaImage = MediaImage;
+export type TVideo = Video;
+export type TExternalVideo = ExternalVideo;
+export type TModel3d = Model3d;
+export type TMediaNodes = (TMediaImage | TExternalVideo | TVideo | TModel3d)[];
 
 /*
  * Products
--- src/components/client/ProductDetails.client/ProductDetails.client.tsx
+++ src/components/client/ProductDetails.client/ProductDetails.client.tsx
@@ -1,12 +1,15 @@
-import { ProductOptionsProvider } from "@shopify/hydrogen";
+import { ProductOptionsProvider, MediaFile } from "@shopify/hydrogen";
 
 //types
-import { TProduct } from "src/common/types";
+import { TProduct, TMediaNodes } from "src/common/types";
 
 function ProductDetail({ product }: { product: TProduct }) {
   return (
     <ProductOptionsProvider data={product}>
       <section>
+        <div>
+          <ProductGallery media={product.media.nodes} />
+        </div>
         <div>
           <h1>{product.title}</h1>
           <span>{product.vendor}</span>
@@ -22,3 +25,45 @@ function ProductDetail({ product }: { product: TProduct }) {
 }
 
 export { ProductDetail };
+
+// components
+
+function ProductGallery({ media }: { media: TMediaNodes }) {
+  if (!media.length) {
+    return null;
+  }
+  return (
+    <div
+      className={`grid gap-4 overflow-x-scroll grid-flow-col md:grid-flow-row  md:p-0 md:overflow-x-auto md:grid-cols-2 w-screen md:w-full lg:col-span-2`}
+    >
+      {media.map((med, i) => {
+        let extraProps = {};
+        if (med.mediaContentType === "MODEL_3D") {
+          extraProps = {
+            interactionPromptThreshold: "0",
+            ar: true,
+            loading: "eager",
+            disableZoom: true,
+          };
+        }
+        let image;
+        if ("image" in med) {
+          image = {
+            ...med.image,
+            altText: med.alt || "Product image",
+          };
+        }
+
+        const data = {
+          ...med,
+          image: image,
+        };
+        return (
+          <div key={med.id}>
+            <MediaFile data={data} {...extraProps} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
