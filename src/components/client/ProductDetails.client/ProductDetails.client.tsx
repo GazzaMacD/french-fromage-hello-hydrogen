4ca12ea90d25468f42e55ea146cc88fe016d4187
Gareth Macdonald <macdonald.gareth@gmail.com>
2022-11-08 07:30:04 +0100

    Add product form

--- src/components/client/ProductDetails.client/ProductDetails.client.tsx
+++ src/components/client/ProductDetails.client/ProductDetails.client.tsx
@@ -1,4 +1,10 @@
-import { ProductOptionsProvider, MediaFile } from "@shopify/hydrogen";
+import {
+  ProductOptionsProvider,
+  MediaFile,
+  useProductOptions,
+  ProductPrice,
+  BuyNowButton,
+} from "@shopify/hydrogen";
 
 //types
 import { TProduct, TMediaNodes } from "src/common/types";
@@ -14,6 +20,7 @@ function ProductDetail({ product }: { product: TProduct }) {
           <h1>{product.title}</h1>
           <span>{product.vendor}</span>
         </div>
+        <ProductForm product={product} />
         <div>
           <div
             dangerouslySetInnerHTML={{ __html: product.descriptionHtml }}
@@ -27,15 +34,80 @@ function ProductDetail({ product }: { product: TProduct }) {
 export { ProductDetail };
 
 // components
+function ProductForm({ product }) {
+  const { options, selectedVariant } = useProductOptions();
+  const isOutOfStock = !selectedVariant?.availableForSale || false;
+  return (
+    <form>
+      {
+        <div>
+          {options &&
+            options.map(({ name, values }) => {
+              if (values.length === 1) {
+                return null;
+              }
+              return (
+                <div key={name}>
+                  <legend>{name}</legend>
+                  <div>
+                    <OptionRadio name={name} values={values} />
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      }
+      <div>
+        <ProductPrice
+          priceType="compareAt"
+          variantId={selectedVariant.id}
+          data={product}
+        />
+        <ProductPrice variantId={selectedVariant.id} data={product} />
+      </div>
+      <div>
+        {isOutOfStock ? (
+          <span>Available in 2-3 weeks</span>
+        ) : (
+          <BuyNowButton variantId={selectedVariant.id}>
+            <span>Buy it now</span>
+          </BuyNowButton>
+        )}
+      </div>
+    </form>
+  );
+}
+function OptionRadio({ values, name }) {
+  const { selectedOptions, setSelectedOption } = useProductOptions();
+  return (
+    <>
+      {values.map((value) => {
+        const checked = selectedOptions[name] === value;
+        const id = `option-${name}-${value}`;
+        return (
+          <label key={id} htmlFor={id}>
+            <input
+              type="radio"
+              id={id}
+              name={`option[${name}]`}
+              value={value}
+              checked={checked}
+              onChange={() => setSelectedOption(name, value)}
+            />
+            <div>{value}</div>
+          </label>
+        );
+      })}
+    </>
+  );
+}
 
 function ProductGallery({ media }: { media: TMediaNodes }) {
   if (!media.length) {
     return null;
   }
   return (
-    <div
-      className={`grid gap-4 overflow-x-scroll grid-flow-col md:grid-flow-row  md:p-0 md:overflow-x-auto md:grid-cols-2 w-screen md:w-full lg:col-span-2`}
-    >
+    <div>
       {media.map((med, i) => {
         let extraProps = {};
         if (med.mediaContentType === "MODEL_3D") {
